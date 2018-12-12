b097f702e16ae08faa07e63fc2d31e05d9ad737d
Alexandra Fren <35177685+alexandrafren@users.noreply.github.com>
2018-12-11 20:00:38 -0500

    Create temperature.cpp

---
+++ temperature.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  
+  double tempf = 32;
+  
+  double tempc;
+  
+  tempc = (tempf - 32)/1.8;
+  
+  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+  
+  
+}
