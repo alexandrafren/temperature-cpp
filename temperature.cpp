88f37c8e78afe8551ef99eafe6f6f759bdb74a98
Alexandra Fren <35177685+alexandrafren@users.noreply.github.com>
2018-12-11 20:19:20 -0500

    Allows for user input of temperature

--- temperature.cpp
+++ temperature.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
 int main() {
-  
-  double tempf = 32;
-  
+  double tempf;
   double tempc;
   
-  tempc = (tempf - 32)/1.8;
+  // Ask the user
+  std::cout << "Enter the temperature in Farenheit:\n";
+  std::cin >> tempf;
+  
+  
+  tempc = (tempf - 32) / 1.8;
   
   std::cout << "The temp is " << tempc << " degrees Celsius.\n";
-  
   
 }
